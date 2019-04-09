55009b6cd7b0f39895762fe1c58e99d9a59c7d56
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-09 14:05:09 -0700

    PEP8 fixes

--- 1-pac_web_static.py
+++ 1-pac_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''
-Fabric script that generates a `.tgz` archive from the contents of
-the `web_static` folder of your AirBnB Clone repo, using the function `do_pack`.
+Fabric script that generates a `.tgz` archive from
+the contents of the `web_static` folder of your
+AirBnB Clone repo, using the function `do_pack`.
 '''
 from os import makedirs
 from os.path import exists
