7d9ab7420f1412c2691d8af2a369f35d3d600bb1
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-09 23:40:08 -0700

    Updating nginx config w/ printf

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,7 +18,7 @@ printf %s "server {
 	index index.html index.htm;
 
 	# Make site accessible from http://localhost/
-	server_name localhost;
+	# server_name localhost;
 
 	location /hbnb_static {
 		alias /data/web_static/current;
@@ -29,11 +29,10 @@ printf %s "server {
 		return 301 https://www.fernando.ai;
 	}
 
-
 	location / {
 		# First attempt to serve request as file, then
 		# as directory, then fall back to displaying a 404.
-		try_files $uri $uri/ =404;
+		#try_files $uri $uri/ =404;
 		# Uncomment to enable naxsi on this location
 		# include /etc/nginx/naxsi.rules
 	}
