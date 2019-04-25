8049025b4ad43c4440c8fed2edb592dffecca9ee
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-24 23:49:26 -0700

    Importing models

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the state class"""
 import os
+import models
 from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
